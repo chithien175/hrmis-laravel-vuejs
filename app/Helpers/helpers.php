ba2118ca18b6cc25d4fc2595f578db31256f3a70
Thien Pham <chithien175@gmail.com>
2019-07-23 16:53:16 +0700

    update

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * @param
+ * menuName: string
+ * options: array(id, class, sub_id, sub_class)
+ * @return string
+ */
 if (!function_exists('menu')) {
     function menu($menuName, $options)
     {
